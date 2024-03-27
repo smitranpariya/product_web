31648b8f2e2bda88eaffb9f340ed9d013e4767c4
Blacky <smitranpariya30@gmail.com>
2024-03-28 02:09:28 +0530

    upadate

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from flask_pymongo import PyMongo 
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/product_web"
+mongo = PyMongo(app) 
+db = mongo.db
+
+
+
+@app.route("/",methods=["POST" , "GET"])
 def login():
-    return render_template("login_page.html")
+        return render_template("login_page.html")
+
+@app.route("/do_login",methods=["POST","GET"])
+def do_login():
+        
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        form_input = {"email":email,
+                      "password":password}
+        db.admin.insert_one(form_input)
+        return  'Form submitted successfully. password: {}, Email: {}'.format(password, email)
+
 
 app.run(debug=True)
